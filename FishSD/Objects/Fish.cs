4130e0e3e0f9d9e28583b5e5f2f03cc26b16e872
Tyler Malkus <malkustj@gmail.com>
2014-11-09 19:27:02 -0500

    Added Cat's shitty shit

--- FishSD/Objects/Fish.cs
+++ FishSD/Objects/Fish.cs
@@ -11,6 +11,9 @@ namespace FishSD.Objects
         public bool Alive { get; set; }
         public double Energy { get; set; }
         protected Dictionary<string, double> Attributes { get; set; }
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int range = 100;
 
         public Fish(Location location) : base(location)
         {
@@ -34,7 +37,41 @@ namespace FishSD.Objects
             g.Alive = false;
             Energy += g.Energy * Attributes["energy-gain"];
         }
+        public void Update()
+        {
+            if (!Alive) return;
 
+            if (Energy > Attributes["max-energy"]) Energy = Attributes["max-energy"];
+        }
+
+        public int[] Move()
+        {
+            switch (new Random().Next(4))
+            {
+                case 0:
+                    this.LocationY = LocationY++; //move up
+                    break;
+                case 1:
+                    this.LocationY = LocationY--; //move down
+                    break;
+                case 2:
+                    this.LocationX = LocationX++; //move right
+                    break;
+                case 3:
+                    this.LocationX = LocationX--; //move left
+                    break;
+                default:
+                    break;
+            }
+            if (LocationX > range)
+                this.LocationX = 0;
+            if (LocationY > range)
+                this.LocationY = 0;
+
+            int[] location = { LocationX, LocationY };
+            return location;
+
+        }
         
     }
 }
