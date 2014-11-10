467434456a00b534dbfa74455384c20e6461ab0f
Tyler Malkus <malkustj@gmail.com>
2014-11-09 19:58:19 -0500

    WORK!!!

---
+++ FishSD/Objects/Location.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishSD.Objects
+{
+    class Location
+    {
+        int x { get; set; }
+        int y { get; set; }
+
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Location(int maxSize)
+        {
+            var r = new Random();
+            this.x = r.Next(maxSize);
+            this.y = r.Next(maxSize);
+        }
+        public void Move(string command)
+        {
+            switch (command)
+            {
+                case ("UP"):
+                    y++; //move up
+                    break;
+                case ("DOWN"):
+                    y--; //move down
+                    break;
+                case ("RIGHT"):
+                    x++; //move right
+                    break;
+                case ("LEFT"):
+                    x--; //move left
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void Move()
+        {
+            switch (new Random().Next(4))
+            {
+                case (0):
+                    y++; //move up
+                    break;
+                case (1):
+                    y--; //move down
+                    break;
+                case (2):
+                    x++; //move right
+                    break;
+                case (3):
+                    x--; //move left
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
